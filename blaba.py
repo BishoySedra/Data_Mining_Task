af00208d38c2ee556664aa6f9ea01b524c3df83f
mohamed hossam <100865584+muhammedhossam@users.noreply.github.com>
2023-11-30 10:35:55 +0200

    Add files via upload

--- blaba.py
+++ blaba.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from itertools import combinations 
 
-Horizontal_Data = 0
-freq_item_set = []
+
+
 def read_data_from_excel(file_path):
 # read by default 1st sheet of an excel file
         data = pd.read_excel(file_path)
@@ -29,70 +28,83 @@ def read_data_from_excel(file_path):
 def check_data_format(pathfile):
     orginal  = {}
     df = read_data_from_excel(pathfile)
-    print(df.iloc[0])
     for i in range(len(df)):
         orginal[df.iloc[i][1]] = []
     for i in range(len(df)):
-        orginal[df.iloc[i][1]].append([df.iloc[i][0]])
+        orginal[df.iloc[i][1]].append(df.iloc[i][0])
     return orginal
     
-print(check_data_format('Horizontal_Format.xlsx'))
+
 df = check_data_format('Horizontal_Format.xlsx')
+# print(df)
 
-# def support_removing(data,min_support): #if length 1 
-def generate_frequent_item_sets(data,min_support):  
-    support_items={}
-    frequent_item_set=[]
-    for item in df.keys():  
-        support_items[item] =len(df[item])  #C1
-        if len(item) == 1: 
-            print(f"item {item} Support : {len(df[item])}")
-            
-        
-    for item, support in support_items.items():
-        if support >= min_support:
-                frequent_item_set.append(item)#L1
-    freq_item_set.append(list(frequent_item_set)) #list of set
-    # print(freq_item_set)
-    # print(frequent_item_set) 
-    index = 2
+def data_pruning(df,min_sup):
+    res = {}
+    for i in df.items():
+        # m : 1 2 3
+        if len(i[1]) >= min_sup:
+            res[i[0]] = i[1]
+    
+    return res
+
+# jimmy nitron
+def fun(v1, v2):
+    i = 0
+    j = 0
+    v = []
+    while i < len(v1) and j < len(v2):
+        while i < len(v1) and j < len(v2) and v1[i] != v2[j]:
+            if v1[i] < v2[j]:
+                i += 1
+            else:
+                j += 1
+        if i < len(v1) and j < len(v2) and v1[i] == v2[j]:
+            v.append(v1[i])
+            i += 1
+            j += 1
+    return v
+
+def Key_item_comp(df,min_sub):
+
+    keys = []
+    king = {}
+    cnt = 1
+
+
+    # data pruning
+    df = data_pruning(df,min_sub)
+    keys = list(df.keys())
+    n = len(keys)
+
+
+    # compination
+    for i in keys:
+        for j in keys[cnt:n]:
+            if len(i) > 1:
+                if i[0:len(i) - 1] != j[0:len(j) - 1]:
+                    break
+            # (M,O) : 1, 2, 3
+            candidate = fun(df[i],df[j])
+            name = str(i + j[len(j) - 1:])
+            king[name] = candidate
+        cnt = cnt + 1
+    
+    # for frequent set more than of equal 2
+    king = data_pruning(king,min_sub)
+    return king
+
+
+
+def run(df,min_sup):
+    king = []
+    queen = df
+    king.append(data_pruning(queen,min_sup))
     while True:
-            elements = set()
-            for item_set in freq_item_set[index - 2]:
-                for item in item_set:
-                    elements.add(item)
-            print("###################################")
-            # print(elements)
-            elements_combinations = list(combinations(elements, index))
-            print(elements_combinations)
-            # print(support_items.items())
-            for item_set in elements_combinations: 
-                for item in range(index-1):
-                    common = set(map(tuple,(df[item_set[item]]))).intersection(set(map(tuple,(df[item_set[item+1]])))) # Convert lists of lists to sets of tuples to make them hashable
-                support_items[item_set] = len(common)
-            print("###################################")
-                
-            # print(support_items.items())
-            for item, support in support_items.items():
-                if len(item) == index:
-                    if support >= min_support:
-                        frequent_item_set.append(item)
-            # print(frequent_item_set)
-            freq_item_set.append(list(frequent_item_set))
-            count = 0
-            for item in range(index-1):
-                first_list= freq_item_set[count]
-                freq_item_set[index - 1]=list(set(freq_item_set[index - 1]).difference(first_list))
-                count+=1
-            
-            print(freq_item_set)
-            print(len(freq_item_set[-1]))
-            if len(freq_item_set[-1]) == 0:
-                break
-            
-            index+=1
+        queen = Key_item_comp(queen,min_sup)
+        if queen == {}:
+            break
+        king.append(queen)
+    return king
 
-
-# support_removing(df,2)    
-generate_frequent_item_sets(df,3)
-
+df = run(df,3)
+print(df)
