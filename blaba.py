beb5ca4b44a9e7a3d28c07621a25e5177ed7b0eb
mohamed hossam <100865584+muhammedhossam@users.noreply.github.com>
2023-12-04 19:54:40 +0200

    Add files via upload

--- blaba.py
+++ blaba.py
@@ -1,5 +1,10 @@
+
+
+
+
 import pandas as pd
 from itertools import combinations
+
 
 def read_data_from_excel(file_path):
     data = pd.read_excel(file_path)
@@ -28,14 +33,15 @@ def check_data_format(pathfile):
         orginal[df.iloc[i][1]].append(df.iloc[i][0])
     return orginal
 
+
 def data_pruning(df,min_sup):
     res = {}
     for i in df.items():
-        # m : 1 2 3
         if len(i[1]) >= min_sup:
             res[i[0]] = i[1]
-
     return res
+
+
 
 
 def Key_item_comp(df,min_sub):
@@ -54,18 +60,35 @@ def Key_item_comp(df,min_sub):
     # compination
     for i in keys:
         for j in keys[cnt:n]:
-            if len(i) > 1:
-                if i[0:len(i) - 1] != j[0:len(j) - 1]:
+            if len(i) >= 1:
+                if not check_condition_join(i,j):
                     break
             # (M,O) : 1, 2, 3
             candidate = generate_candidate_item_sets(df[i],df[j])
-            name = str(i + j[len(j) - 1:])
+            name=str(join(i,j))
             king[name] = candidate
+            # print(candidate)
         cnt = cnt + 1
 
     # for frequent set more than of equal 2
     king = data_pruning(king,min_sub)
     return king
+
+
+def check_condition_join(str1,str2):
+    list1 = str1.split(',')
+    list2 = str2.split(',')
+    i=j=0
+    x=0
+    for item1, item2 in zip(list1[:-1], list2[:-1]):
+        if item1!=item2:
+            return False
+    return True
+
+def join(i,j):
+    list1 = j.split(',')
+    last_str=list1[len(list1)-1]
+    return i+","+last_str
 
 
 def generate_candidate_item_sets(v1, v2):
@@ -84,120 +107,190 @@ def generate_candidate_item_sets(v1, v2):
             j += 1
     return v
 
+
+
 def generate_subsequences(sequence):
     subsequences = []
     for r in range(1, len(sequence) + 1):
         # Use combinations to generate all subsequences of length r
         subsequences.extend(combinations(sequence, r))
-    return ["".join(subseq) for subseq in subsequences]
-
-
-def generate_association_rules_addition(v):
-    x = len(v[-1])
-    result_pairs = []
-    for i in range(len(v)):
-        for j in range(len(v)):
-            if v[i] != v[j] and len(v[i]) + len(v[j]) == x:
-                b = helper_fun(v[i], v[j])
-                if b:
-                    result_pairs.append((v[i], v[j]))
-    return result_pairs
+    return [",".join(subseq) for subseq in subsequences]
 
 
 def helper_fun(str1, str2):
-    if str1 > str2:
-        str1, str2 = str2, str1
-    s = set(str2)
-    for ch in str1:
+    list1 = str1.split(',')
+    list2 = str2.split(',')
+    s = set(list1)
+    for ch in list2:
         if ch in s:
             return False
     return True
 
-def generate_association_rules(frequent_itemsets, min_confidence):
+def generate_association_rules_addition(v):
+    x = len(v[-1])
+    result_pairs = []
+    # print(v)
+    for i in range(len(v)):
+        for j in range(len(v)):
+            # here
+            temp=v[i]+","+v[j]
+            if v[i] != v[j] and len(temp) == x:
+                b = helper_fun(v[i]+",", v[j])
+                # print(temp,v)
+                if b:
+                    # print(v[i]+",",v[j])
+
+                    result_pairs.append((v[i]+",",v[j]))
+    return result_pairs
+
+def convert_to_one_list(frequent_itemsets):
+    result_dict = {}
+    for d in frequent_itemsets:
+        for key, value in d.items():
+            if key in result_dict:
+                result_dict[key].extend(value)
+            else:
+                result_dict[key] = value
+    return result_dict
+
+all_rules = []
+strong_rules = []
+lift_list=[]
+def generate_association_rules(frequent_itemsets, min_confidence,number_trans):
     levels = len(frequent_itemsets)
-    print("levels : " , levels)
+    # print("levels : " , levels)
     # print(frequent_itemsets)
     if levels == 1:
-        print("No Association Rules can be generated!\n")
+        print("No Association Rules can be generated!")
         return
-    all_rules = []
-    strongrule = []
-    real_association_rules=[]
+
+
+    result_dict=convert_to_one_list(frequent_itemsets)
+    # print(frequent_itemsets)
+    one_list=convert_to_one_list(frequent_itemsets)
     print(frequent_itemsets)
     for level in range(1,levels):
         # print("level in for loop : ",level)
         for itemset, support in frequent_itemsets[level].items():
                 subsequences = []
                 my_item=itemset
-                subsequences=generate_subsequences(my_item)
+                # print(my_item)
+                my_list=my_item.split(',')
+                subsequences=generate_subsequences(my_list)
+                # print("sub")
                 # print(subsequences)
                 x=generate_association_rules_addition(subsequences)
-                print(x)
-                real_association_rules=real_association_rules+x
-                # you cam mapp first element only {m,k} {k,m} km mot exsits
                 orginal_list=x[0]
+                # print(x)
                 result_string=""
+                # print(x)
                 for char in orginal_list:
                     result_string=result_string+str(char)
-                stringOfAll = ''
-                for z in x[0]:
-                    stringOfAll = stringOfAll + z
-                print('----------------------------')
-                print(stringOfAll)
-                list_lift = []
-                for i in range(0,int(len(x)/2)):
-                    # print('blabla : ',x[i])
-                    support_all_item = len(frequent_itemsets[len(stringOfAll)-1][stringOfAll])
-                    support_partof_item = len(frequent_itemsets[len(x[i][0])-1][x[i][0]])
-                    second_support_partof_item = len(frequent_itemsets[len(x[i][1])-1][x[i][1]])
-                    list_lift.append(1/(support_all_item / support_partof_item * second_support_partof_item))
-                    print("HHHHEEEE")
-                    print(list_lift)
 
+                # result_string=str(result_string[1:])
+                # print(result_string)
+
+                # first_comma_index = result_string.find(',')
+                # print(result_string)
+
+
+                # # print('----------------------------')
                 for rules_inx in x:
-                    support_all_item = len(frequent_itemsets[len(result_string)-1][result_string])
-                    support_partof_item = len(frequent_itemsets[len(rules_inx[0])-1][rules_inx[0]])
+                    # print("rules_inx",rules_inx)
+                    # comma
+                    string1=rules_inx[0]
+                    string2=rules_inx[1]
+                    real_string1=string1[:len(string1)-1]
+                    # print("real_string1",real_string1)
+                    # # print("string1",string1)
+                    # print("string2",string2)
+
+                    support_all_item = len(one_list[result_string])
+                    support_partof_item = len(one_list[real_string1])
                     confidence = support_all_item / support_partof_item
-                    rule = {"first_item": rules_inx[0], "second_item": rules_inx[1], "confidence": confidence}
+                    # print("support_all_item",support_all_item)
+                    # print("support_partof_item",support_partof_item)
+                    # print("confidence",confidence)
+                    rule = {"first_item": real_string1, "second_item": string2, "confidence": confidence}
                     all_rules.append(rule)
                     if confidence >= min_confidence:
-                        rule = {"first_item": rules_inx[0], "second_item": rules_inx[1], "confidence": confidence}
-                        strongrule.append(rule)
-                        
+                        rule = {"first_item": real_string1, "second_item": string2, "confidence": confidence}
+                        strong_rules.append(rule)
+                temp=0
+                # print(x)
+                for rules_inx1 in x:
+                    if temp == len(x)/2:
+                        break;
+                    # print(rules_inx1)
+                    string1=rules_inx1[0]
+                    string2=rules_inx1[1]
+                    real_string1=string1[:len(string1)-1]
+                    support_all_item = len(one_list[result_string])
+                    support_partof_item1 = len(one_list[real_string1])
+                    support_partof_item2 = len(one_list[string2])
 
-    # for rule in strongrule:
-    #     print(
-    #         f"Strong Rules: {rule['first_item']} => {rule['second_item']}, Confidence: {rule['confidence']} , Lift:{rule['lift']}"
-    #     )
-    print('#'*50)
-    for rule in all_rules:
-        print(
-            f"All Rules: {rule['first_item']} => {rule['second_item']}, Confidence: {rule['confidence']}"
-        )
-    
+                    lift = (support_all_item/number_trans) / ((support_partof_item1/number_trans)*(support_partof_item2/number_trans))
+                    print(lift)
+                    
+                    rule = {"first_item": real_string1 , "second_item": string2 , "lift": lift}
+                    # print(rule)
+                    lift_list.append(rule)
+                    temp=temp+1
+
+
+
+
+        # return
+
+def print_all():
+    print("\nThe strong association rules: ")
+    for rule in strong_rules:
+        print(f"{rule['first_item']} => {rule['second_item']}  , Confidence= {rule['confidence']}")
+    print('-'*50)
+    print("\nThe lift : ")
+    for rule in lift_list:
+        print(f"{rule['first_item']} => {rule['second_item']}  , lift = {rule['lift']}")
+        if rule['lift'] == 1:
+            print("they are independent")
+        elif rule['lift'] < 1:
+            print("they are  dependent , Negative Correlation")
+        else :
+            print("they are  dependent , Positive Correlation")
+
+
+    print('-'*50)
+
 
 
 def run(df, min_sup):
     king = []
     queen = df
     king.append(data_pruning(queen, min_sup))
+    print(data_pruning(queen, min_sup))
     while True:
         queen = Key_item_comp(queen, min_sup)
+        # print (queen)
         if queen == {}:
             break
         king.append(queen)
     return king
+# pathfile="blabla.xlsx"
+pathfile="Horizontal_Format.xlsx"
+df = pd.read_excel(pathfile)
+number_trans=len(df)
+
+df=check_data_format(pathfile)
+df=run(df,3)
+print('-'*50)
+generate_association_rules(df, 0.7,number_trans)
+
+# x=Key_item_comp(df,2)
+print_all()
 
 
-## running area
-df = check_data_format("Horizontal_Format.xlsx")
-df = run(df,3)
 
-print(df)
-generate_association_rules(df,1)
-# for level in range(len(result)):
-#     print(f"L{level + 1}:")
-#     for item, TID_SET in result[level].items():
-#         print(item, f"Support is {len(TID_SET)}\n")
 
-# generate_association_rules(df, 0.5)
+
+
+
+
